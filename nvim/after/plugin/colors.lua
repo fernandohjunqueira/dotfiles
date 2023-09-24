2220a37785d2315da98bba7349bf0c6937921b91
Fernando Henrique <fernandohjunqueira@proton.me>
2023-09-24 15:09:03 -0300

    Set neovim background transparent to match terminal background color

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -3,8 +3,8 @@ function ColorMyPencils(color)
 	vim.cmd.colorscheme(color)
 
   -- Uncomment the lines below if you want to use transparent background
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
